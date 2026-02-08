b6cc5e28336a001c8a18b47a131a4f239238d49d
Bs0Dd <vladislav.z.kz@gmail.com>
2026-02-08 02:24:07 +0100

    Extra tabulation

--- STMP/fatfs/ff.c
+++ STMP/fatfs/ff.c
@@ -3836,7 +3836,7 @@ FRESULT f_read_byte (
 		fp->fptr++;
 		LEAVE_FF(fs, FR_OK);
 	}
-	
+
 	if ((fp->fptr % SS(fs)) == 0) {
 		uint32_t csect = (fp->fptr / SS(fs) & (fs->csize - 1));
 		if (csect == 0) {
